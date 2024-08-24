5509e8c4444baa7ebd11dd0fa0c022797affa0e7
Mikku0 <mikolaj080803@gmail.com>
2024-08-24 23:53:36 +0200

    prototype solution

--- PP0502B.cs
+++ PP0502B.cs
@@ -7,6 +7,21 @@ namespace
 {
     public class PP0502B
     {
-        
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++) {
+            string numbers = Console.ReadLine();
+            int nums = int.Parse(numbers[0].ToString());
+            int[] list = new int[nums];
+            string[] numsArray = numbers.Split(' ');
+            int index = nums - 1;
+            for (int j = 1; j < nums; j++){
+                list[j] = int.Parse(numsArray[index]);
+                index--;
+            }
+            foreach(var item in list)
+            {
+                Console.Write(item.ToString() + ' ');
+            }
+        }
     }
 }
